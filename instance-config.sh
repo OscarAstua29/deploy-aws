d4f221a00e4b7c1a5cd0aa51607a6bdce7a44d9d
OscarAstua29 <oscarastua2002@gmail.com>
2024-12-18 23:59:20 -0600

    workflow v36

--- instance-config.sh
+++ instance-config.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
 # Actualizar los paquetes del sistema
-echo "Actualizando el sistema..."
+echo "UPDATING SYSTEM"
 sudo yum update -y
 
 # Instalar Docker
-echo "Instalando Docker..."
+echo "DOCKER INSTALATION"
 sudo yum install -y docker
 
 # Verificar la versión de Docker
-echo "Verificando la versión de Docker..."
+echo "DOCKER VERSION"
 docker --version
 
 # Iniciar el servicio de Docker
-echo "Iniciando el servicio de Docker..."
+echo "DOCKER START"
 sudo service docker start
 
 # Añadir el usuario 'ec2-user' al grupo 'docker' para usar Docker sin sudo
-echo "Añadiendo ec2-user al grupo docker..."
+echo "ADDING EC2 TO DOCKER GROUP"
 sudo usermod -a -G docker ec2-user
 
 # Instalar Docker Compose
-echo "Instalando Docker Compose..."
+echo "DOCKER-COMPOSE INSTALATION"
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
 # Hacer ejecutable el binario de Docker Compose
-echo "Configurando permisos para Docker Compose..."
+echo "CHANGING DOCKER PERMISSIONS"
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Verificar la versión de Docker Compose
-echo "Verificando la versión de Docker Compose..."
+echo "DOCKER-COMPOSE VERSION"
 sudo docker-compose version
 
 # Ejecutar Docker Compose en modo 'detached' (en segundo plano)
-echo "Iniciando Docker Compose..."
+echo "INITIALIZING DOCKER COMPOSE"
 sudo docker-compose up -d
 
-echo "Docker y Docker Compose han sido instalados y configurados correctamente."
+echo "SCRIPT ENDS"
